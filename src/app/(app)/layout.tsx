413fa1d9abdd82a51f133525c6d547830d609dde
Bass Eem <basseem363@gmail.com>
2025-07-04 17:51:24 +0000

    This is empty,  side bar didn't close why

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -19,7 +19,7 @@ export default function AppLayout({
       </Sidebar>
       <SidebarInset>
         <header className="flex h-14 items-center gap-4 border-b bg-card px-6">
-          <SidebarTrigger className="md:hidden" />
+          <SidebarTrigger />
           <div className="flex-1">
             {/* Can add breadcrumbs or page title here */}
           </div>
